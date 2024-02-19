ef4b6e152f247300f38dc491040f82620c18c83f
Oles Savluk <oles.savluk@smarkets.com>
2024-02-19 17:32:18 +0000

    chore: build v0.2.1

---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,8 @@
+interface SeonInterface {
+    setSessionId(sessionId: string): Promise<boolean>;
+    setLoggingEnabled(enabled: boolean): Promise<boolean>;
+    getFingerprintBase64(): Promise<string>;
+}
+declare const _default: SeonInterface;
+export default _default;
+//# sourceMappingURL=index.d.ts.map
